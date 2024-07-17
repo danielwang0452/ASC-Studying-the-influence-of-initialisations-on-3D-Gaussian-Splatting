960620a921960dfd8c3b0df1107200023f798079
Daniel Wang <u7918232@anu.edu.au>
2024-07-17 16:53:21 +1000

    test

--- test_SimpleGaussianModel.py
+++ test_SimpleGaussianModel.py
@@ -34,7 +34,7 @@ class SimpleGaussianModel:
         self.spatial_lr_scale = 0
         self.setup_functions()
 
-    def create_from_pcd(self, pcd: BasicPointCloud, spatial_lr_scale: float = 1):
+    def create_from_pcd(self, pcd, spatial_lr_scale: float = 1):
         opacity = 0.1
 
         self.spatial_lr_scale = spatial_lr_scale
@@ -148,7 +148,6 @@ def strip_lowerdiag(L):
 
 def strip_symmetric(sym):
     return strip_lowerdiag(sym)
-
 
 class BasicPointCloud(NamedTuple):
     points: np.array
