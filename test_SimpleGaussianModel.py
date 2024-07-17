cf9eb62fae11d4e4195d38f97be0c40524a953a9
Daniel Wang <u7918232@anu.edu.au>
2024-07-17 18:20:41 +1000

    test

--- test_SimpleGaussianModel.py
+++ test_SimpleGaussianModel.py
@@ -53,6 +53,8 @@ class SimpleGaussianModel:
         print("Number of points at initialisation : ", fused_point_cloud.shape[0])
 
         dist2 = torch.clamp_min(self.distCUDA2(torch.from_numpy(np.asarray(pcd.points)).float().cuda()), 0.0000001)
+        print(dist2.shape)
+        print(dist2[..., None].shape)
         scales = torch.log(torch.sqrt(dist2))[..., None].repeat(1, 3)
         rots = torch.zeros((fused_point_cloud.shape[0], 4), device="cuda")
         rots[:, 0] = 1
